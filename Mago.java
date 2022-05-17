68f34178981032385a9bfef1811f69cf5fa91c44
Gustavo87463 <99288745+Gustavo87463@users.noreply.github.com>
2022-05-17 16:38:28 -0300

    Add files via upload

---
+++ Mago.java
@@ -0,0 +1,41 @@
+public class Mago {
+
+    
+    String nome;
+    int forca;
+    int destreza;
+    int agilidade;
+    int inteligencia;
+    int carisma;
+    int resistencia;
+    int vida;
+    int estamina;
+    int sorte;
+    int mana;
+
+    
+    int atacar(int valorDado){
+        int valorAtaque = 0;
+        if(valorDado < 2){
+            System.out.println("O ataque falhou!!!");
+        }else{
+            valorAtaque = this.forca+valorDado;
+        }
+        return valorAtaque;
+
+    }
+    
+    void defender(int valorAtaque){
+        if(valorAtaque <= this.resistencia){
+            System.out.println("O ataque foi defendido!!!");
+        }else{
+            int danoCausado = valorAtaque - this.resistencia;
+            System.out.println("O dano causado foi de: " + danoCausado);
+            //this.vida = this.vida - danoCausado
+            this.vida -= danoCausado;
+
+        }
+    }
+    
+
+}
